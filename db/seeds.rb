c524a5dbbb03bc7fd89070b843dbe8f9688a846a
antoine lebeau <antoine.lebeau@gmail.com>
2024-02-21 11:26:02 +0100

    yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Religion::Bible.quote
+  )
+  article.save
+end
